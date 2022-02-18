ad8c77fd8915c82423928a1a2494680fff0387e5
Kenan Kadic <kkadic@midtownhi.com>
2022-02-18 12:05:13 -0600

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card :loading="thinking">
+        <v-app-bar color="green" elevation="0" dark>
+          <span class="display-1">Customer Information</span>
+          <v-spacer></v-spacer>
+          <v-icon>mdi-account</v-icon>
+        </v-app-bar>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
