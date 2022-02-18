3cf01fcf1b63e12582815b389d937812084e9e9f
Kenan Kadic <kkadic@midtownhi.com>
2022-02-18 12:09:36 -0600

    v-card added

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,12 @@
     <v-row>
       <v-col>
         <v-card :loading="thinking">
-        <v-app-bar color="green" elevation="0" dark>
+        <v-app-bar color="blue" elevation="0" dark>
           <span class="display-1">Customer Information</span>
           <v-spacer></v-spacer>
           <v-icon>mdi-account</v-icon>
         </v-app-bar>
+          <v-card-title>I am a card</v-card-title>
         </v-card>
       </v-col>
     </v-row>
